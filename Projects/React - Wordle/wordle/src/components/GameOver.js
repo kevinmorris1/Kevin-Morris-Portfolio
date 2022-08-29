9ea732b4eeace8449be485ae67294bd876f2af43
kevinmorris1 <kdm0030@mix.wvu.edu>
2022-08-29 13:16:24 -0400

    Wordle Game Update

--- Projects/React - Wordle/wordle/src/components/GameOver.js
+++ Projects/React - Wordle/wordle/src/components/GameOver.js
@@ -10,8 +10,8 @@ function GameOver() {
 
     return (
         <div className='gameOver'>
-            <h3>{gameOver.guessedWord ? "You Win!" : " You Failed"}</h3>
-            <h1>Correct: {correctWord}</h1>
+            <h3>{gameOver.guessedWord ? "You won the game!" : "You failed"}</h3>
+            <h1>Correct Word: {correctWord}</h1>
             <div>{gameOver.guessedWord && (<h3>You guessed in {currAttempt.attempt} attemps</h3>)}</div>
             <div><button onClick={playAgain}>Play Again</button></div>
         </div>
